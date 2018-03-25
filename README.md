a5413f80a964a2ef4b99e53b68c7df82637376de
Boss Daddi <bossdaddi@Bosss-iMac.local>
2018-03-25 16:30:44 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center">
+    <a href="https://github.com/yiisoft" target="_blank">
+        <img src="https://avatars1.githubusercontent.com/u/37776879?s=400&u=bee08167c572fe02d475dd84c9f62fab4c473888&v=4" height="100px">
+    </a>
+    <h1 align="center">Projects Yii2 Basic</h1>
+    <br>
+</p>
+
+This application will be designed to track projects using Yii 2 Basic Project Template
---
+++ config/web.php
@@ -0,0 +1,92 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+                'enableUnconfirmedLogin' => true,
+                'confirmWithin' => 21600,
+                'cost' => 12,
+                'admins' => ['admin']
+        ],
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'K6v00DhnP3qTzAD18rHtVpotx7B-pOHX',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'dektrium\user\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => FALSE,
+                 'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                    'host' => 'smtp.gmail.com',
+                    'username' => 'dbateit@gmail.com',
+                    'password' => 'Bossman18',
+                    'port' => 587,
+                    'encryption' => 'tls'
+                ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+}
+
+return $config;
